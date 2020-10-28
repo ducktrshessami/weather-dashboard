47d322a22e632078b5aee1e183d6e722b9b69c0c
ducktrshessami <jwee761@gmail.com>
2020-10-27 19:12:33 -0700

    start page display

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,12 +1,17 @@
 (function() { // Enclose scope for debugging
 
     var recentSearches;
+    var display = false;
 
     /*
     Initialize the page
     */
     function init() {
         getHistory();
+
+        if (recentSearches.length) {
+            displayCity(recentSearches[recentSearches.length - 1])
+        }
     }
 
     /*
@@ -21,6 +26,12 @@
     function storeHistory() {
         let relevant = recentSearches.map(item => item.loc);
         localStorage.setItem("recentSearches", JSON.stringify(relevant));
+    }
+
+    /*
+    */
+    function displayCity(city) {
+        
     }
 
 })();
