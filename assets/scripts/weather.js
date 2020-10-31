0e108e5ecd0648c9ec0e453bcfede5353ce86db8
ducktrshessami <jwee761@gmail.com>
2020-10-31 15:38:06 -0700

    commenting

--- assets/scripts/weather.js
+++ assets/scripts/weather.js
@@ -1,12 +1,20 @@
 var apikey;
 
 /*
+Set your OpenWeather API key
+@params:
+key - a string containing your key
 */
 function setWeatherApiKey(key) {
     apikey = key;
 }
 
 /*
+Get UVI severity color hex
+@params:
+uvi - a number containing the UVI
+@returns:
+a string containing the color hex
 */
 function getUVSeverityColor(uvi) {
     if (uvi < 3) {
@@ -27,6 +35,12 @@ function getUVSeverityColor(uvi) {
 }
 
 /*
+Interfaces wtih OpenWeather's Current Weather and One Call APIs to get current and future weather
+conditions. Then, format the data into a single object to be displayed.
+@params:
+query - a string containing the location to get data for
+@returns:
+a Promise that resolves in an object containing the formatted data
 */
 function getWeatherFormatted(query) {
     return new Promise(function(resolve, reject) {
@@ -36,7 +50,7 @@ function getWeatherFormatted(query) {
                     loc: current.name,
                     cache: {
                         timestamp: Date.now(),
-                        current: {
+                        current: { // Format current conditions
                             icon: {
                                 src: `https://openweathermap.org/img/wn/${current.weather[0].icon}@2x.png`,
                                 alt: current.weather[0].main
@@ -44,9 +58,9 @@ function getWeatherFormatted(query) {
                             temp: current.main.temp,
                             humid: current.main.humidity,
                             wind: current.wind.speed,
-                            uv: future.daily[0].uvi
+                            uv: future.daily[0].uvi // Why doesn't the Current Weather API have UVI?
                         },
-                        forecast: future.daily.map(item => {
+                        forecast: future.daily.map(item => { // Format forecast
                             let day = new Date(item.dt * 1000);
                             return {
                                 date: `${day.getMonth() + 1}/${day.getDate()}/${day.getFullYear()}`,
@@ -57,7 +71,7 @@ function getWeatherFormatted(query) {
                                 temp: item.temp.day,
                                 humid: item.humidity
                             };
-                        }).slice(1, 6)
+                        }).slice(1, 6) // Only get the next 5 days' forecast
                     }
                 });
             });
@@ -66,6 +80,13 @@ function getWeatherFormatted(query) {
 }
 
 /*
+Call OpenWeather's Current Weather API
+@params:
+query - a string containing the location to search for
+fail - the error handling callback function because for some reason jQuery's ajax has different
+        error handling than a standard Promise
+@returns:
+what appears to be a Promise, but also bypasses Promise rejection unless otherwise specified
 */
 function getWeatherCurrent(query, fail) {
     return $.ajax({
@@ -76,6 +97,14 @@ function getWeatherCurrent(query, fail) {
 }
 
 /*
+Call OpenWeather's One Call API
+@params:
+lat - a string containing latitude coordinates for the query location
+lon - a string containing longitude coordinates for the query location
+fail - the error handling callback function because for some reason jQuery's ajax has different
+        error handling than a standard Promise
+@returns:
+what appears to be a Promise, but also bypasses Promise rejection unless otherwise specified
 */
 function getWeatherForecast(lat, lon, fail) {
     return $.ajax({
